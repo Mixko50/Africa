b38c357bb423eb84f632bebc5bab5364cd5c80df
Mixko <apisit.mixko@mail.kmutt.ac.th>
2022-01-04 01:52:40 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Mixko on 4/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
